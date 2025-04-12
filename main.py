1382da92e58498d4a7075282f7e8a8ad305e10e5
riadh323 <riadhfarrah8@gmail.com>
2025-04-12 14:36:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,5 +32,8 @@ def webhook():
         print("Fehler:", e)
     return "OK", 200
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
